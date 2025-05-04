05e68fba53822b74d393ceb900e17e9c40916084
thien <phanthothien204@gmail.com>
2025-05-04 15:42:45 +0700

    add board

---
+++ src/step-definitions/board.steps.ts
@@ -0,0 +1,6 @@
+import {Given, When, Then} from '@cucumber/cucumber';
+import {expect, Page} from '@playwright/test';
+import { BoardPage } from '../page-objects/BoardPage';
+
+let boardPage: BoardPage;
+
