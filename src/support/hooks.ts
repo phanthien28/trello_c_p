80d5518a6a3cfcea28cf36a902ef26da9a6de476
thien <phanthothien204@gmail.com>
2025-05-14 10:02:54 +0700

    Create Jenkinsfile

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -16,7 +16,7 @@ setDefaultTimeout(60000);
 
 BeforeAll(async function() {
     browser = await chromium.launch({
-        headless: false,
+        headless: true,
         slowMo: 1000,
         args: ['--start-maximized'],
     });
